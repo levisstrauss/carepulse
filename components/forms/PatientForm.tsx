c4a5ccf1163a89d990a9648e38c71a59b97237b7
levisstrauss <levisstrauss11@yahoo.com>
2024-09-20 06:51:40 -0400

    Implemented user form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,8 +1,97 @@
-import React from 'react'
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "@/components/CustomFormField";
+import SubmitButton from "@/components/SubmitButton";
+import {useState} from "react";
+import {UserFormValidation} from "@/lib/validation";
+import { useRouter } from "next/navigation";
+
+export enum FormFieldType {
+    INPUT = 'input',
+    TEXTAREA = 'textarea',
+    PHONE_INPUT = 'phoneInput',
+    CHECKBOX = 'checkbox',
+    DATE_PICKER = 'datePicker',
+    SELECT = 'select',
+    SKELETON = 'skeleton',
+}
 
 const PatientForm = () => {
+    const router = useRouter();
+    const [isLoading, setisLoading] = useState(false);
+
+    // Default validation check
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+        },
+    })
+
+    // Submitting the form
+    async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+        setisLoading(true);
+        try {
+            // const userData = {
+            //     name,
+            //     email,
+            //     phone,
+            // }
+            // const user = createUser(userData);
+            // if(user) router.push(`/patients/${user.$id}/register`);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <div>PatientForm</div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+                <section className="mb-12 space-y-4">
+                    <h1 className="header">Hi there 👋</h1>
+                    <p className="text-dark-700">Get started with appointments.</p>
+                </section>
+                <CustomFormField
+                    // @ts-ignore
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="name"
+                    label="Full name"
+                    placeholder="John Doe"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="user"
+                />
+                <CustomFormField
+                    // @ts-ignore
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="Johndoe@codemon.pro"
+                    iconSrc="/assets/icons/email.svg"
+                    iconAlt="email"
+                />
+                <CustomFormField
+                    // @ts-ignore
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    control={form.control}
+                    name="phone"
+                    label="Phone Number"
+                    placeholder="(555) 123-4257"
+                />
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+            </form>
+        </Form>
     )
 }
+
+
 export default PatientForm
